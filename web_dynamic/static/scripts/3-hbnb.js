6e1c681ddaa0ec82a524688aa840ea7e0635c6a7
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-04-07 01:33:51 +0200

    t5

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -18,4 +18,38 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
+
+  $.ajax('http://0.0.0.0:5001/api/v1/places_search', {
+    data: JSON.stringify({}),
+    contentType: 'application/json',
+    type: 'POST',
+    success: function (response) {
+      response.sort((a, b) => {
+        // Sort by place name in ascending order
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+      for (const place of response) {
+        const article = $('<article></article>');
+        article.append('<div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div>');
+        article.append('<div class="information"><div class="max_guest">' + place.max_guest +
+          ' Guest' + (place.max_guest !== 1 ? 's' : '') +
+          '</div><div class="number_rooms">' + place.number_rooms +
+          ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') +
+          '</div><div class="number_bathrooms">' + place.number_bathrooms +
+          ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') +
+          '</div></div>');
+        const ownerRequest = $.get('http://0.0.0.0:5001/api/v1/users/' + place.user_id);
+        $.when(ownerRequest).done(function (ownerResponse) {
+          article.append('<div class="user"><b>Owner:</b> ' + ownerResponse.first_name + ' ' + ownerResponse.last_name + '</div>');
+          article.append('<div class="description">' + place.description + '</div>');
+          $('section.places').append(article);
+        });
+      }
+    },
+    error: function (xhr, status) {
+      console.log('error');
+    }
+  });
 });
